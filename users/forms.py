753a2286aede39ca419bb5f2dd699e0e7ff1147e
ilyadev750 <petrovich76007600kz@gmail.com>
2022-12-27 23:30:47 +0300

    working with form validation

--- users/forms.py
+++ users/forms.py
@@ -3,6 +3,12 @@ from django import forms
 from .models import User
 
 class UserForm(forms.ModelForm):
+    city = forms.CharField(widget=forms.Textarea(attrs={
+        "class": "new class",
+        "placeholder": "description",
+        "rows": 20,
+        "cols": 10
+    }))
     class Meta:
         model = User
         fields = [
@@ -12,6 +18,15 @@ class UserForm(forms.ModelForm):
             'city',
             'sex'
         ]
+
+    def clean_sex(self, *args, **kwargs):
+        sex = self.cleaned_data.get("sex")
+        if "Male" in sex:
+            return sex
+        elif "Female" in sex:
+            return sex
+        else:
+            raise forms.ValidationError("Wrong sex")
 
 class RawUserForm(forms.Form):
     first_name = forms.CharField(widget=forms.TextInput(attrs={
--- users/views.py
+++ users/views.py
@@ -11,30 +11,30 @@ def user_details_view(request):
     }
     return render(request, "users/users_details.html", context)
 
-# def user_new_view(request):
-#     form = UserForm(request.POST or None)
-#     if form.is_valid():
-#         form.save()
-#
-#     context = {
-#         'form': form
-#     }
-#     return render(request, "users/users_another_details.html", context)
 def user_new_view(request):
-    my_form = RawUserForm()
-    if request.method == "POST":
-        my_form = RawUserForm(request.POST)
-        if my_form.is_valid():
-            print(my_form.cleaned_data)
-            User.objects.create(**my_form.cleaned_data)
-        else:
-            print(my_form.errors)
+    form = UserForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+
     context = {
-        "form": my_form
+        'form': form
     }
-    # new_first_name = request.POST.get('first_name')
-    # print(new_first_name)
-    # context = {}
     return render(request, "users/users_another_details.html", context)
+# def user_new_view(request):
+#     my_form = RawUserForm()
+#     if request.method == "POST":
+#         my_form = RawUserForm(request.POST)
+#         if my_form.is_valid():
+#             print(my_form.cleaned_data)
+#             User.objects.create(**my_form.cleaned_data)
+#         else:
+#             print(my_form.errors)
+#     context = {
+#         "form": my_form
+#     }
+#     # new_first_name = request.POST.get('first_name')
+#     # print(new_first_name)
+#     # context = {}
+#     return render(request, "users/users_another_details.html", context)
 
 # Create your views here.
